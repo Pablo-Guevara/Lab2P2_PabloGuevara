18f411b92215180375fbe4c951b7d1387370b4f7
Pablo Ronaldo Guevara <Ronaldo@192.168.2.35>
2021-07-30 13:54:58 -0600

    #2->Clase creada y agregar y listar

--- src/lab/pkg2p2_pabloguevara/Lab2P2_PabloGuevara.java
+++ src/lab/pkg2p2_pabloguevara/Lab2P2_PabloGuevara.java
@@ -1,9 +1,115 @@
 package lab.pkg2p2_pabloguevara;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Lab2P2_PabloGuevara {
 
     public static void main(String[] args) {
-        
+        Scanner leer = new Scanner(System.in);
+
+        ArrayList<pymes> listaPymes = new ArrayList();
+        int opcion = 0;
+
+        String nombre;
+        String slogan;
+        String rtn;
+        int cantidadEmpleados;
+        String descripcion;
+        int añoFundacion;
+        int cantidadSocios;
+        String nombreGerenteG;
+        String ubicacion;
+        int ventasUltimoAño;
+        String sectorSocial="";
+        int sectorSocialInt;
+        long capitalSemilla;
+
+        while (opcion != 7) {
+            System.out.println("-----MENU-----");
+            System.out.println("1-Crear pyme");
+            System.out.println("2-Listar pymes");
+            System.out.println("3-Modificar pyme");
+            System.out.println("4-Eliminar pyme");
+            System.out.println("Ingrese su opcion: ");
+            opcion = leer.nextInt();
+            leer.nextLine();
+            if (opcion == 1) {
+                System.out.println("Ingrese nombre de la pyme: ");
+                nombre=leer.nextLine();
+                System.out.println("Ingrese slogan de la pyme: ");
+                slogan=leer.nextLine();
+                System.out.println("Ingrese rtn de la pyme: ");
+                rtn=leer.next();
+                System.out.println("Ingrese cantidad de empleados de la pyme: ");
+                cantidadEmpleados=leer.nextInt();
+                leer.nextLine();
+                System.out.println("Ingrese descripcion de la pyme: ");
+                descripcion=leer.nextLine();
+                System.out.println("Ingrese año de fundacion de la pyme: ");
+                añoFundacion=leer.nextInt();
+                leer.nextLine();
+                System.out.println("Ingrese cantidad de socios de la pyme: ");
+                cantidadSocios=leer.nextInt();
+                leer.nextLine();
+                System.out.println("Ingrese nombre del gerente general de la pyme: ");
+                nombreGerenteG=leer.nextLine();
+                System.out.println("Ingrese ubicacion de la pyme: ");
+                ubicacion=leer.nextLine();
+                System.out.println("Ingrese ventas del ultimo año de la pyme: ");
+                ventasUltimoAño=leer.nextInt();
+                leer.nextLine();
+                System.out.println("Ingrese el sector social de la pyme: ");
+                System.out.println("1-Transporte");
+                System.out.println("2-Medicina");
+                System.out.println("3-Educacion");
+                System.out.println("4-Ventas");
+                System.out.println("5-Servicios financieros");
+                System.out.println("6-Servicios de consultoria");
+                System.out.println("7-Ecologia");
+                System.out.println("8-Ciencias juridicas");
+                sectorSocialInt=leer.nextInt();
+                if (sectorSocialInt==1) {
+                    sectorSocial="Transporte";
+                }
+                if (sectorSocialInt==2) {
+                    sectorSocial="Medicina";
+                }
+                if (sectorSocialInt==3) {
+                    sectorSocial="Educacion";
+                }
+                if (sectorSocialInt==4) {
+                    sectorSocial="Ventas";
+                }
+                if (sectorSocialInt==5) {
+                    sectorSocial="Servicios financieros";
+                }
+                if (sectorSocialInt==6) {
+                    sectorSocial="Servicios de consultoria";
+                }
+                if (sectorSocialInt==7) {
+                    sectorSocial="Ecologia";
+                }
+                if (sectorSocialInt==8) {
+                    sectorSocial="Ciencias juridicas";
+                }
+                System.out.println("Ingrese capital semilla de la pyme: ");
+                capitalSemilla=leer.nextLong();
+                
+                listaPymes.add(new pymes(nombre, slogan, rtn, cantidadEmpleados, descripcion, añoFundacion, cantidadSocios, nombreGerenteG, ubicacion, ventasUltimoAño, sectorSocial, capitalSemilla));
+                
+            }
+            
+            if (opcion==2) {
+                String salida = "";
+                for (Object t : listaPymes) {
+                    if (t instanceof pymes) {
+                        salida+=listaPymes.indexOf(t)+ "- " + t + "\n";
+                    }
+                }
+                System.out.println(salida);
+            }
+        }
     }
-    
+
 }
--- src/lab/pkg2p2_pabloguevara/pymes.java
+++ src/lab/pkg2p2_pabloguevara/pymes.java
@@ -1,14 +1,138 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab.pkg2p2_pabloguevara;
 
-/**
- *
- * @author Ronaldo
- */
 public class pymes {
+
+    private String nombre;
+    private String slogan;
+    private String rtn;
+    private int cantidadEmpleados;
+    private String descripcion;
+    private int añoFundacion;
+    private int cantidadSocios;
+    private String nombreGerenteG;
+    private String ubicacion;
+    private int ventasUltimoAño;
+    private String sectorSocial;
+    private long capitalSemilla;
+
+    public pymes() {
+    }
+
+    public pymes(String nombre, String slogan, String rtn, int cantidadEmpleados, String descripcion, int añoFundacion, int cantidadSocios, String nombreGerenteG, String ubicacion, int ventasUltimoAño, String sectorSocial, long capitalSemilla) {
+        this.nombre = nombre;
+        this.slogan = slogan;
+        this.rtn = rtn;
+        this.cantidadEmpleados = cantidadEmpleados;
+        this.descripcion = descripcion;
+        this.añoFundacion = añoFundacion;
+        this.cantidadSocios = cantidadSocios;
+        this.nombreGerenteG = nombreGerenteG;
+        this.ubicacion = ubicacion;
+        this.ventasUltimoAño = ventasUltimoAño;
+        this.sectorSocial = sectorSocial;
+        this.capitalSemilla = capitalSemilla;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSlogan() {
+        return slogan;
+    }
+
+    public void setSlogan(String slogan) {
+        this.slogan = slogan;
+    }
+
+    public String getRtn() {
+        return rtn;
+    }
+
+    public void setRtn(String rtn) {
+        this.rtn = rtn;
+    }
+
+    public int getCantidadEmpleados() {
+        return cantidadEmpleados;
+    }
+
+    public void setCantidadEmpleados(int cantidadEmpleados) {
+        this.cantidadEmpleados = cantidadEmpleados;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getAñoFundacion() {
+        return añoFundacion;
+    }
+
+    public void setAñoFundacion(int añoFundacion) {
+        this.añoFundacion = añoFundacion;
+    }
+
+    public int getCantidadSocios() {
+        return cantidadSocios;
+    }
+
+    public void setCantidadSocios(int cantidadSocios) {
+        this.cantidadSocios = cantidadSocios;
+    }
+
+    public String getNombreGerenteG() {
+        return nombreGerenteG;
+    }
+
+    public void setNombreGerenteG(String nombreGerenteG) {
+        this.nombreGerenteG = nombreGerenteG;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public int getVentasUltimoAño() {
+        return ventasUltimoAño;
+    }
+
+    public void setVentasUltimoAño(int ventasUltimoAño) {
+        this.ventasUltimoAño = ventasUltimoAño;
+    }
+
+    public String getSectorSocial() {
+        return sectorSocial;
+    }
+
+    public void setSectorSocial(String sectorSocial) {
+        this.sectorSocial = sectorSocial;
+    }
+
+    public long getCapitalSemilla() {
+        return capitalSemilla;
+    }
+
+    public void setCapitalSemilla(long capitalSemilla) {
+        this.capitalSemilla = capitalSemilla;
+    }
+
+    @Override
+    public String toString() {
+        return "pymes: {" + "Nombre=" + nombre + ", Slogan=" + slogan + ", rtn=" + rtn + ", Cantidad de empleados=" + cantidadEmpleados + ", Descripcion=" + descripcion + ", Año fundacion=" + añoFundacion + ", Cantidad de socios=" + cantidadSocios + ", Nombre del gerente general=" + nombreGerenteG + ", ubicacion=" + ubicacion + ", ventas del ultimo año=" + ventasUltimoAño + ", sector social=" + sectorSocial + ", capital semilla=" + capitalSemilla + '}';
+    }
+    
     
 }
